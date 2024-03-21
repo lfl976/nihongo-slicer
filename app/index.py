e4a8044a2d5c47b55fb3f46ec15dd44eb98cd400
liufulin <976529466@qq.com>
2024-03-21 18:11:57 +0900

    chore: update test

--- app/index.py
+++ app/index.py
@@ -1,58 +1,12 @@
-from flask import Flask, request, Response, jsonify
-from flask_cors import CORS
-import MeCab
+from flask import Flask
 
 app = Flask(__name__)
-mecab = MeCab.Tagger()
-CORS(app)
-
-katakana_to_hiragana = {
-    'ア': 'あ', 'イ': 'い', 'ウ': 'う', 'エ': 'え', 'オ': 'お',
-    'ァ': 'ぁ', 'ィ': 'ぃ', 'ゥ': 'ぅ', 'ェ': 'ぇ', 'ォ': 'ぉ',
-    'カ': 'か', 'キ': 'き', 'ク': 'く', 'ケ': 'け', 'コ': 'こ',
-    'ガ': 'が', 'ギ': 'ぎ', 'グ': 'ぐ', 'ゲ': 'げ', 'ゴ': 'ご',
-    'サ': 'さ', 'シ': 'し', 'ス': 'す', 'セ': 'せ', 'ソ': 'そ',
-    'ザ': 'ざ', 'ジ': 'じ', 'ズ': 'ず', 'ゼ': 'ぜ', 'ゾ': 'ぞ',
-    'タ': 'た', 'チ': 'ち', 'ツ': 'つ', 'テ': 'て', 'ト': 'と',
-    'ダ': 'だ', 'ヂ': 'ぢ', 'ヅ': 'づ', 'デ': 'で', 'ド': 'ど',
-    'ッ': 'っ',
-    'ナ': 'な', 'ニ': 'に', 'ヌ': 'ぬ', 'ネ': 'ね', 'ノ': 'の',
-    'ハ': 'は', 'ヒ': 'ひ', 'フ': 'ふ', 'ヘ': 'へ', 'ホ': 'ほ',
-    'バ': 'ば', 'ビ': 'び', 'ブ': 'ぶ', 'ベ': 'べ', 'ボ': 'ぼ',
-    'パ': 'ぱ', 'ピ': 'ぴ', 'プ': 'ぷ', 'ペ': 'ぺ', 'ポ': 'ぽ',
-    'マ': 'ま', 'ミ': 'み', 'ム': 'む', 'メ': 'め', 'モ': 'も',
-    'ャ': 'ゃ', 'ヤ': 'や', 'ュ': 'ゅ', 'ユ': 'ゆ', 'ョ': 'ょ', 'ヨ': 'よ',
-    'ラ': 'ら', 'リ': 'り', 'ル': 'る', 'レ': 'れ', 'ロ': 'ろ',
-    'ヮ': 'ゎ', 'ワ': 'わ', 'ヰ': 'ゐ', 'ヱ': 'ゑ', 'ヲ': 'を',
-    'ン': 'ん'
-}
-
-
-def katakana_to_hiragana_convert(text):
-    return ''.join(katakana_to_hiragana.get(char, char) for char in text)
 
 
 @app.route('/')
-def home():
-    return 'Hello, World PY!'
-
-
-@app.route('/tokenize', methods=['GET'])
-def tokenize():
-    text = request.args.get('text')
-    if not text:
-        return jsonify({"error": "No text provided"}), 400
-    result = mecab.parse(text)
-
-    print(result)
-    res_array = result.splitlines()[:-1]
-
-    res = [{'word': parts[0], 'reading': katakana_to_hiragana_convert(parts[1])} for parts in (item.split() for item in res_array)]
-
-    print(res)
-    return jsonify(res)
+def hello_world():  # put application's code here
+    return 'Hello World! test'
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
-
+    app.run()
